4e57fb0366ba7c9726fd200e32b6eb586f4258bb
ggsspace <ggccsa00@gmail.com>
2026-03-11 07:09:57 -0500

    pagina de inicio

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,5 +1,24 @@
+import "./Home.css";
+
 const Home = () => {
-  return <h1>Inicio</h1>;
+  return (
+
+    <section className="hero-content">
+        <h1 className="hero-title">
+          Innovación que se viste: El <span className="hero-highlight">talento</span> del CDMC en <span className="hero-highlight">tus manos</span>
+        </h1>
+        
+        <p className="hero-description">
+          Explora una colección exclusiva nacida en el corazón del Centro de Diseño 
+          y Manufactura del Cuero. Desde calzado de alta resistencia hasta 
+          marroquinería de autor y prendas con sello de innovación. Cada pieza es 
+          el resultado de la excelencia técnica y la pasión de nuestros aprendices.
+        </p>
+
+        <button className="hero-cta-btn">Explorar Productos</button>
+      </section>
+
+  );
 };
 
 export default Home
