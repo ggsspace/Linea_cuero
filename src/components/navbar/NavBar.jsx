1c893c35eeaad0db5dffe601dd4a9f1814a1591c
ggsspace <ggccsa00@gmail.com>
2026-04-08 10:41:44 -0500

    se agregó el contenido a la página de eventos

---
+++ src/components/navbar/NavBar.jsx
@@ -0,0 +1,52 @@
+import { NavLink } from "react-router-dom";
+import "./NavBar.css";
+import senaLogo from "../../assets/logo_SENA.png";
+import loginIcon from "../../assets/login.png";
+import carritoIcon from "../../assets/carrito.png";
+
+const NavBar = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar-container">
+        
+        <NavLink to = "/">
+        <button className="home-shortcut-btn">
+          <div className="navbar-logo">
+          <img src={senaLogo} alt="SENA" className="logo-img" />
+          <div className="logo-text">
+            <span className="logo-title">SENA</span>
+            <span className="logo-subtitle">CDMC</span>
+          </div>
+        </div>
+        
+        </button>
+        
+        </NavLink>
+        
+        
+
+        <ul className="navbar-links">
+          <li><NavLink to="/" end>Inicio</NavLink></li>
+          <li><NavLink to="/products">Productos</NavLink></li>
+          <li><NavLink to="/calendar">Calendario</NavLink></li>
+          <li><NavLink to="/events">Eventos</NavLink></li>
+
+        </ul>
+
+        <div className="navbar-icons">
+          <NavLink to="/login" className="icon-btn">
+            <img src={loginIcon} alt="Login" className="nav-icon-img" />
+          </NavLink>
+          <NavLink to="/cart" className="icon-btn">
+            <img src={carritoIcon} alt="Carrito" className="nav-icon-img" />
+          </NavLink>
+
+          
+
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
---
+++ src/pages/cart/Cart.jsx
@@ -0,0 +1,13 @@
+import react from "react";
+import "./Cart.css";
+
+const  Cart = () => {
+  return (
+    <main>
+      <h1>Carrito</h1>
+
+    </main>
+  );
+};
+export default Cart;
+
